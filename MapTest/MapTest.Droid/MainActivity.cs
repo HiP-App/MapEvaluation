378594a351d78f417ed842fd503114305ee2e325
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2016-11-07 12:47:47 +0100

    Test project created

---
+++ MapTest/MapTest.Droid/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MapTest.Droid
+{
+	[Activity (Label = "MapTest.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+	    protected override void OnCreate (Bundle bundle)
+	    {
+	        base.OnCreate (bundle);
+
+	        // Set our view from the "main" layout resource
+	        SetContentView (Resource.Layout.Main);
+
+	        // Get our button from the layout resource,
+	        // and attach an event to it
+	        Button button = FindViewById<Button> (Resource.Id.myButton);
+
+	        button.Click += delegate {
+	            button.Text = string.Format ("{0} clicks!", count++);
+	        };
+
+	        using (var stream = new FileInfo (@"mnt/shared/TMP/map_01.pbf").OpenRead ())
+	        {
+                MyClass.Do (stream);
+	        }
+	    }
+
+	}
+}
+
+
---
+++ MapTest/MapTest/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Itinero;
+using Itinero.IO.Osm;
+using Itinero.Osm.Vehicles;
+
+namespace MapTest
+{
+    public class MyClass
+    {
+
+        public static void Do (Stream stream)
+        {
+            var routerDb = new RouterDb();
+            var router = new Router(routerDb);
+            
+            routerDb.LoadOsmData(stream, Vehicle.Car);
+
+            // calculate a route.
+            var route = router.Calculate(Vehicle.Car.Fastest(),
+                51.72070116f, 8.74880791f, 51.71584899f, 8.75824928f);
+            var geoJson = route.ToGeoJson();
+        }
+    }
+}
+
